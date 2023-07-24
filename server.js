3bd122328126487af65a8c24bc747bc9c873680a
MaynaCamily <138599679+MaynaCamily@users.noreply.github.com>
2023-07-23 23:44:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const { parseISO, getYear, getMonth, getDate, startOfDay, endOfDay } = require('date-fns');
 const express = require('express');
 const { google } = require('googleapis');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
+const axios = require('axios');
+const querystring = require('querystring');
 
 // Configuração do banco de dados PostgreSQL
 const pool = new Pool({
@@ -18,9 +19,9 @@ const app = express();
 app.use(bodyParser.json());
 
 const googleConfig = {
-  clientId: '998930018535-2lh1765ss4lm6204qbnv8tjesr678gba.apps.googleusercontent.com',
-  clientSecret: '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',
-  redirectUri: 'http://localhost:8000/oauth2callback'
+  clientId: '998930018535-2lh1765ss4lm6204qbnv8tjesr678gba.apps.googleusercontent.com', // substituir
+  clientSecret: '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', // substituir 
+  redirectUri: '/oauth2callback'// substituir
 };
 
 const oAuth2Client = new google.auth.OAuth2(
@@ -39,17 +40,16 @@ app.get('/oauth2callback', async (req, res) => {
     const { tokens } = await oAuth2Client.getToken(code);
     const accessToken = tokens.access_token;
     const refreshToken = tokens.refresh_token;
-
-    console.log('Token de acesso:', accessToken);
-    console.log('Token de atualização:', refreshToken);
-
-
-    res.send('Autorização concluída com sucesso!');
+    res.send(`
+      Token de acesso: ${accessToken}
+      Token de atualização: ${refreshToken}
+    `);
   } catch (error) {
     console.error('Erro ao obter token de acesso:', error);
-    res.send('200');
+    res.status(500).send('Erro ao obter token de acesso.');
   }
 });
+
 
 // Rota para iniciar o processo de autorização
 app.get('/authorize', (req, res) => {
@@ -61,26 +61,64 @@ app.get('/authorize', (req, res) => {
   res.redirect(authorizeUrl);
 });
 
+//códigos de acesso clickup
+const clientIDCK = '8J3KDYR653YIZ2EFBGWFRVRWGOXNYS45'; // substituir 
+const clientSecretCK = '3UOTM1H7B2VHMHOJ82625VE6QGQSN0BRVOGCLP11IT38R5Y70KC9ONWHCCYMW2KA'; // substituir
+const redirectURI = `` // substituir
+
+//autorização clickup
+app.get('/clickupAuth', (req, res) => {
+  try {
+    res.redirect(`https://app.clickup.com/api?client_id=${clientIDCK}&redirect_uri=${redirectURI}`);
+  } catch (error) {
+    console.error('Erro ao redirecionar para a página de autorização do ClickUp:', error);
+    res.status(500).send('Ocorreu um erro ao redirecionar para a página de autorização do ClickUp.');
+  }
+});
+
+//callback clickup
+app.get('/callback', async (req, res) => {
+  const { code } = req.query;
+
+  try {
+    const tokenResponse = await axios.post('https://api.clickup.com/api/v2/oauth/token', querystring.stringify({
+      client_id: clientIDCK,
+      client_secret: clientSecretCK,
+      code,
+      grant_type: 'authorization_code',
+      redirect_uri: redirectURI,
+    }));
+
+    res.send(`Token de acesso: ${tokenResponse.data.access_token}`);
+  } catch (error) {
+    console.error('Erro ao trocar código de autorização por token de acesso:', error.message);
+    res.status(500).send('Ocorreu um erro ao obter o token de acesso.');
+  }
+});
+
 // Rota para solicitações do webhook
 app.post('/webhook', async (req, res) => {
   res.status(200).end();
 
   const resourceId = req.headers['x-goog-resource-id'];
 
-  const query = 'SELECT acess_token, token_refresh, calendar_id, user_id_clickup FROM base WHERE resource_id = $1';
+  const query = 'SELECT access_token, token_refresh, calendar_id, email, user_id_clickup, token_clickup FROM base WHERE resource_id = $1';
   const values = [resourceId];
   const result = await pool.query(query, values);
 
   if (result.rows.length > 0) {
-    const accessToken = result.rows[0].acess_token;
+    const accessToken = result.rows[0].access_token;
     const refreshToken = result.rows[0].token_refresh;
     const calendarId = result.rows[0].calendar_id;
     const user_id_clickup = result.rows[0].user_id_clickup;
+    const tokenClickup = result.rows[0].token_clickup;
+    const email = result.rows[0].email;
+
 
     const googleConfig = {
-      clientId: '998930018535-2lh1765ss4lm6204qbnv8tjesr678gba.apps.googleusercontent.com',
-      clientSecret: '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',
-      apiKey: 'AIzaSyD1KXWd0RFfjvadaVmWRXUm6Ae8yU8Lxqg'
+      clientId: '998930018535-2lh1765ss4lm6204qbnv8tjesr678gba.apps.googleusercontent.com', // substituir
+      clientSecret: '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', // substituir
+      apiKey: 'AIzaSyD1KXWd0RFfjvadaVmWRXUm6Ae8yU8Lxqg' // substituir
     };
 
     const oAuth2Client = new google.auth.OAuth2(
@@ -105,7 +143,7 @@ app.post('/webhook', async (req, res) => {
       const events = response.data.items;
 
       if (events.length) {
-        await processEvents(events, user_id_clickup);
+        await processEvents(events, user_id_clickup, tokenClickup, email);
       }
     });
   }
@@ -114,13 +152,13 @@ app.post('/webhook', async (req, res) => {
 const processingTasksMap = new Map();
 
 // função de processo de notificação e extração de dados para tarefa
-async function processEvents(events, user_id_clickup) {
+async function processEvents(events, user_id_clickup, tokenClickup, email) {
   for (const event of events) {
     const eventId = event.id;
     const titleParts = event.summary.split(' - ');
     const eventName = titleParts[0];
-    const folderId = titleParts[1];
-    const listName = titleParts[2];
+    const spaceName = titleParts[1];
+    const projectId = titleParts[2];
     const created = event.created;
     const status = event.status;
     const updated = event.updated;
@@ -131,10 +169,11 @@ async function processEvents(events, user_id_clickup) {
     const dueDate = event.end.date;
     const startDate = event.start.date;
     const recurringEventId  = event.recurringEventId;
+    
 
     const eventData = {
-      folderId: folderId,
-      listName: listName,
+      folderName: projectId,
+      spaceName: spaceName,
       eventId: eventId,
       recurringEventId: recurringEventId,
       name: eventName,
@@ -145,7 +184,22 @@ async function processEvents(events, user_id_clickup) {
       startDateTime: startDateTime,
       dueDate: dueDate,
       startDate: startDate,
+      tokenClickup: tokenClickup,
+      email: email,
     };
+
+    try {
+      const userClickUpId = await getUserIdFromClickUp(email, tokenClickup);
+      eventData.user_id_clickup = userClickUpId; 
+  
+      const updateQuery = 'UPDATE base SET user_id_clickup = $1 WHERE email = $2';
+      const updateValues = [userClickUpId, email];
+      await pool.query(updateQuery, updateValues);
+  
+    } catch (error) {
+      console.error(`Error getting user id from ClickUp for email "${email}": ${error.message}`);
+      return;
+    }
 
     if (recurringEventId) {
       console.log(`Evento é recorrente (recurringEventId: ${recurringEventId}), não será criada nenhuma tarefa.`);
@@ -187,15 +241,22 @@ async function processEvents(events, user_id_clickup) {
 
 // função para salvar o evento 
 async function saveEvent(eventId, created, status, updated) {
-  const eventExists = await checkEventExistence(eventId);
-  if (!eventExists) {
-    const query = 'INSERT INTO eventos (id_evento, id_task, created, status, updated) VALUES ($1, $2, $3, $4, $5)';
-    const values = [eventId, null, created, status, updated];
-    await pool.query(query, values);
-  } else {
-    const updateQuery = 'UPDATE eventos SET created = $2, status = $3, updated = $4 WHERE id_evento = $1';
-    const updateValues = [eventId, created, status, updated];
-    await pool.query(updateQuery, updateValues);
+  try {
+    const checkExistenceQuery = 'SELECT * FROM eventos WHERE id_evento = $1';
+    const checkExistenceValues = [eventId];
+    const result = await pool.query(checkExistenceQuery, checkExistenceValues);
+
+    if (result.rows.length === 0) {
+      const insertQuery = 'INSERT INTO eventos (id_evento, created, status, updated) VALUES ($1, $2, $3, $4)';
+      const insertValues = [eventId, created, status, updated];
+      await pool.query(insertQuery, insertValues);
+      console.log(`Event ${eventId} saved to the database.`);
+    } else {
+      console.log(`Event ${eventId} already exists in the database. Skipping insertion.`);
+    }
+  } catch (error) {
+    console.error(`Error saving event ${eventId} to the database: ${error.message}`);
+    throw error;
   }
 }
 
@@ -237,7 +298,7 @@ function convertDateTimeEventToInt64(date) {
 
 //criação de da tarefa no clickup
 async function createTaskClickup(eventData) {
-  const { folderId, listName, eventId, hasGoaceVcGuests, user_id_clickup, name, recurringEventId} = eventData;
+  const { folderName, spaceName, eventId, hasGoaceVcGuests, user_id_clickup, name, recurringEventId, tokenClickup} = eventData;
   
   const isAllDayEvent = !eventData.dueDateTime && !eventData.startDateTime;
 
@@ -250,6 +311,13 @@ async function createTaskClickup(eventData) {
   }
 
   try {
+
+    const spaceId = await getSpaceIdFromTeam(spaceName,tokenClickup);
+
+    const folderId = await getFolderIdFromName(spaceId, folderName,tokenClickup);
+
+    const listId = await getListIdFromFolder(folderId, folderName, tokenClickup);
+
     const query = 'SELECT id_task FROM eventos WHERE id_evento = $1';
     const values = [eventId];
     const result = await pool.query(query, values);
@@ -261,8 +329,6 @@ async function createTaskClickup(eventData) {
     }
 
     const assignees = [user_id_clickup];
-
-    const listId = await getListIdFromName(folderId, listName);
 
     if (hasGoaceVcGuests) {
       const goaceVcGuests = eventData.attendees.filter(guest => guest.email.endsWith('goace.vc'));
@@ -278,7 +344,7 @@ async function createTaskClickup(eventData) {
       {
         method: 'POST',
         headers: {
-          'Authorization': 'pk_44248054_AIOJURUIS2YVAX00VNWMAVY7MPGP7P3J',
+          'Authorization': `Bearer ${tokenClickup}`,
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
@@ -303,14 +369,17 @@ async function createTaskClickup(eventData) {
   }
 }
 
-async function getListIdFromName(folderId, listName) {
+async function getSpaceIdFromTeam(spaceName, tokenClickup) {
   try {
+    // team id ace: 
+    const teamId = '12926935';
+
     const resp = await fetch(
-      `https://api.clickup.com/api/v2/folder/${folderId}/list`,
+      `https://api.clickup.com/api/v2/team/${teamId}/space`,
       {
         method: 'GET',
         headers: {
-          'Authorization': 'pk_44248054_AIOJURUIS2YVAX00VNWMAVY7MPGP7P3J',
+          'Authorization': `Bearer ${tokenClickup}`,
           'Content-Type': 'application/json',
         },
       }
@@ -318,7 +387,87 @@ async function getListIdFromName(folderId, listName) {
 
     if (resp.ok) {
       const data = await resp.json();
-      const list = data.lists.find(list => list.name === listName);
+      const space = data.spaces.find(space => space.name.trim() === spaceName.trim());
+      if (space) {
+        return space.id;
+      } else {
+        throw new Error(`Space with name "${spaceName}" not found in team with ID "${teamId}".`);
+      }
+    } else {
+      throw new Error(`Error fetching spaces from team with ID "${teamId}": ${await resp.text()}`);
+    }
+  } catch (error) {
+    console.error(`Error getting spaceId from name for team "${teamId}" and spaceName "${spaceName}": ${error.message}`);
+    throw error;
+  }
+}
+
+async function getFolderIdFromName(spaceId, folderName, tokenClickup) {
+  try {
+    const resp = await fetch(
+      `https://api.clickup.com/api/v2/space/${spaceId}/folder`,
+      {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${tokenClickup}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    if (resp.ok) {
+      const data = await resp.json();
+      let folderId;
+      if (/\d/.test(folderName)) {
+        const regex = new RegExp(folderName, 'i');
+        const folder = data.folders.find(folder => regex.test(folder.name));
+        if (folder) {
+          folderId = folder.id;
+        } else {
+          throw new Error(`Folder with name "${folderName}" not found in space with ID "${spaceId}".`);
+        }
+      } else {
+        const folder = data.folders.find(folder => folder.name === folderName);
+        if (folder) {
+          folderId = folder.id;
+        } else {
+          throw new Error(`Folder with name "${folderName}" not found in space with ID "${spaceId}".`);
+        }
+      }
+      return folderId;
+    } else {
+      throw new Error(`Error fetching folders from space with ID "${spaceId}": ${await resp.text()}`);
+    }
+  } catch (error) {
+    console.error(`Error getting folderId from name for spaceId "${spaceId}" and folderName "${folderName}": ${error.message}`);
+    throw error;
+  }
+}
+
+
+async function getListIdFromFolder(folderId, folderName, tokenClickup) {
+  const listName = /\d/.test(folderName) ? 'cronograma' : 'calendario';
+  console.log('token de acesso', tokenClickup)
+  return await getListIdFromName(folderId, listName.toLowerCase(), tokenClickup);
+}
+
+
+async function getListIdFromName(folderId, listName, tokenClickup) {
+  try {
+    const resp = await fetch(
+      `https://api.clickup.com/api/v2/folder/${folderId}/list`,
+      {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${tokenClickup}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    if (resp.ok) {
+      const data = await resp.json();
+      const list = data.lists.find(list => list.name.toLowerCase() === listName);
       if (list) {
         return list.id;
       } else {
@@ -328,14 +477,16 @@ async function getListIdFromName(folderId, listName) {
       throw new Error(`Error fetching lists from folder with ID "${folderId}": ${await resp.text()}`);
     }
   } catch (error) {
+    console.log('token:', tokenClickup)
     console.error(`Error getting listId from name for folderId "${folderId}" and listName "${listName}": ${error.message}`);
     throw error;
   }
 }
 
+
 // atualização de tarefas no clickup
 async function updateTaskClickup(taskId, eventData) {
-  const {hasGoaceVcGuests, user_id_clickup, name, recurringEventId } = eventData;
+  const {hasGoaceVcGuests, user_id_clickup, name, recurringEventId, tokenClickup } = eventData;
 
   const isAllDayEvent = !eventData.dueDateTime && !eventData.startDateTime;
 
@@ -364,7 +515,7 @@ async function updateTaskClickup(taskId, eventData) {
       {
         method: 'PUT',
         headers: {
-          'Authorization': 'pk_44248054_AIOJURUIS2YVAX00VNWMAVY7MPGP7P3J',
+          'Authorization': `Bearer ${tokenClickup}`,
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
@@ -388,6 +539,37 @@ async function updateTaskClickup(taskId, eventData) {
     console.error(`Erro ao atualizar tarefa ${taskId}: ${error.message}`);
   }
 }
+
+// Busca de user id por email
+async function getUserIdFromClickUp(email, tokenClickup) {
+  try {
+    const resp = await fetch(
+      `https://api.clickup.com/api/v2/user?email=${encodeURIComponent(email)}`,
+      {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${tokenClickup}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    if (resp.ok) {
+      const data = await resp.json();
+      if (data && data.user && data.user.id) {
+        return data.user.id;
+      } else {
+        throw new Error(`User with email "${email}" not found in ClickUp.`);
+      }
+    } else {
+      throw new Error(`Error fetching user from ClickUp with email "${email}": ${await resp.text()}`);
+    }
+  } catch (error) {
+    console.error(`Error getting user id from ClickUp for email "${email}": ${error.message}`);
+    throw error;
+  }
+}
+
 
 
 async function updateEventWithTaskId(eventId, taskId) {
