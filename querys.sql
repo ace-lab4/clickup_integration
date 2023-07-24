5d9ec8fb3d8fa46e3f84e6e376c64d3c4cc8d64e
MaynaCamily <138599679+MaynaCamily@users.noreply.github.com>
2023-07-23 23:43:37 -0300

    Added SQL querys and .env config

---
+++ querys.sql
@@ -0,0 +1,19 @@
+CREATE TABLE base (
+    id SERIAL PRIMARY KEY,
+    user_id_clickup VARCHAR(255)
+    calendar_id VARCHAR(255),
+    email VARCHAR(255),
+    access_token VARCHAR(255),
+    token_refresh TEXT,
+    resource_id VARCHAR(255),
+    token_clickup(255)
+);
+
+CREATE TABLE eventos (
+    id SERIAL PRIMARY KEY,
+    id_evento VARCHAR(255)
+    id_task VARCHAR(255),
+    created VARCHAR(255),
+    status VARCHAR(255),
+    updated VARCHAR(255),
+);
